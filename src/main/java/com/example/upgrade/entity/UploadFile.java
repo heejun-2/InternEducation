273697f27bf7e51963f4d4a8b259d24ede948927
heejun <ghktkf012@naver.com>
2024-01-26 10:55:29 +0900

    refactoring

--- src/main/java/com/example/upgrade/entity/UploadFile.java
+++ src/main/java/com/example/upgrade/entity/UploadFile.java
@@ -6,8 +6,10 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class UploadFile {
+    private int fileId;
     private String uploadFileName;
     private String storeFileName;
+    private int boardId;
 
     public UploadFile(String uploadFileName, String storeFileName) {
         this.uploadFileName = uploadFileName;
